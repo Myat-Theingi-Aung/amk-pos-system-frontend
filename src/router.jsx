afba0ce1d6509b473d902f416fb392c150b19696
lwinleo05 <yinmyatnoelwin1@gmail.com>
2023-08-25 09:30:31 +0630

    contact_us & about_us

--- src/router.jsx
+++ src/router.jsx
@@ -1,11 +1,21 @@
 import { createBrowserRouter } from 'react-router-dom'
 import NotFound from './views/NotFound';
 import Home from './views/Home';
+import ContactUs from './views/ContactUs';
+import AboutUs from './views/AboutUs';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Home />
+  },
+  {
+    path: '/contact-us',
+    element: <ContactUs />
+  },
+  {
+    path: '/about-us',
+    element: <AboutUs />
   },
   {
     path: '*',
