31b50e0fc211b308ff7509aa22513663c323f164
Myat-Theingi-Aung <myattheingiaung1720@gmail.com>
2023-08-25 11:29:04 +0630

    add register route

--- src/router.jsx
+++ src/router.jsx
@@ -4,11 +4,28 @@ import Home from './views/Home';
 import ContactUs from './views/ContactUs';
 import AboutUs from './views/AboutUs';
 import Login from './views/auth/Login';
+import Register from './views/auth/Register'
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Home />
+  },
+  {
+    path: '/login',
+    element: <Login />
+  },
+  {
+    path: '/register',
+    element: <Register />
+  },
+  {
+    path: '/about-us',
+    element: <AboutUs />
+  },
+  {
+    path: '/contact-us',
+    element: <ContactUs />
   },
   {
     path: '*',
